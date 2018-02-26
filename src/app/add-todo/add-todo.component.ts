2a6999f4431c8275607cdbaf292e13e75d0b4847
EugeniaM <maltsevae88@gmail.com>
2018-02-26 22:36:09 +0200

    Add components databinding

---
+++ src/app/add-todo/add-todo.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-add-todo',
+  templateUrl: './add-todo.component.html',
+  styleUrls: ['./add-todo.component.css']
+})
+export class AddTodoComponent implements OnInit {
+  @Output() onAddTodo = new EventEmitter<{newTodoTitle: string, newTodoDesc: string}>();
+  @ViewChild('newDescInput') descriptionInput: ElementRef;
+  // newTodoTitle: string;
+  newTodoDescription: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  addTodo(newTitleInput: HTMLInputElement) {
+    this.onAddTodo.emit({
+      newTodoTitle: newTitleInput.value,
+      newTodoDesc: this.descriptionInput.nativeElement.value
+    });
+  }
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,23 @@
 import { Component } from '@angular/core';
 
+import { Todo } from './models/todo.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  todos = [];
-  newTodoTitle = '';
-  newTodoDescription = '';
+  todos: Array<Todo>;
 
-  onAddTodo() {
+  constructor() {
+    this.todos = [{ title: 'Test1', description: 'Testtest1' }];
+  }
+
+  onTodoAdded(todoData: {newTodoTitle: string, newTodoDesc: string}) {
     this.todos.push({
-      title: this.newTodoTitle,
-      description: this.newTodoDescription
+      title: todoData.newTodoTitle,
+      description: todoData.newTodoDesc
     });
   }
 }
